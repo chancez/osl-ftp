6f963d956ed6940f338e3927a55962328c772a8d
Chance Zibolski <chance.zibolski@gmail.com>
2013-10-10 15:53:51 -0700

    Add configuration parsing

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,6 +1,3 @@
-from datetime import timedelta
-# from celery.schedules import crontab
-
 BROKER_URL = 'amqp://guest@33.33.33.10:5672//'
 CELERY_RESULT_BACKEND = "amqp"
 
@@ -8,12 +5,4 @@ CELERY_IMPORTS = ("mirror.tasks",)
 
 CELERY_TIMEZONE = 'US/Pacific'
 
-CELERYBEAT_SCHEDULE = {
-    'every-minute': {
-        'task': 'mirror.tasks.ShellTask',
-        # 'schedule': crontab(minute='*/1'),
-        'schedule': timedelta(seconds=10),
-        'args': ('ls -l',),
-    },
-}
-
+# CELERYBEAT_SCHEDULE = config_to_schedule()
---
+++ mirror/configuration.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+
+import ConfigParser
+from celery.schedules import crontab
+
+class DictParser(ConfigParser.ConfigParser):
+
+    def as_dict(self):
+        d = dict(self._sections)
+        for section in d:
+            for opt in d[section]:
+                if opt != '__name__':
+                    d[section][opt] = self.get(section, opt)
+            d[section] = dict(self._defaults, **d[section])
+            d[section].pop('__name__', None)
+        return d
+
+def config_to_schedule(raw_config):
+    schedule = {}
+    for section, config in raw_config.as_dict().iteritems():
+        schedule_args = extract_schedule(config)
+        cmd = config.get('cmd', None)
+        schedule.update({
+            section: {
+                'task': 'mirror.tasks.ShellTask',
+                'schedule': crontab(**schedule_args),
+                'args': (cmd,)
+            }
+        })
+
+    return schedule
+
+def extract_schedule(config):
+    crontab_fields = ['minute', 'hour', 'day_of_week',
+                      'day_of_month', 'month_of_year']
+    schedule_args = {}
+    for field in crontab_fields:
+        val = config.get(field, None)
+        if val:
+            arg = {field:val}
+            schedule_args.update(arg)
+    return schedule_args
